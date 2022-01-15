469d2539e4fd6e4bfe502af06c22dea0bf2d0d2c
Pablo Ruiz <pabloruiz@Pablos-MacBook-Pro.local>
2022-01-15 13:36:23 -0800

    login page set up

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </Stack.Navigator> 
+      </NavigationContainer> 
     </View>
   );
 }
