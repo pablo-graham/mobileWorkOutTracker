2f5dd3478a1594e3709918931bf37a370f85726a
Pablo Ruiz <pabloruiz@Pablos-MacBook-Pro.local>
2022-01-14 18:24:00 -0800

    my changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing branch</Text>
+      <Text>Testing</Text>
       <StatusBar style="auto" />
     </View>
   );
